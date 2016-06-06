c11ed3488596399bb1219c420656788a5b7f9d54
pajongjong <jamillarendor@yahoo.com>
2016-06-06 13:58:36 +0800

    DB UPDATE

--- app/models/db/DBUser.java
+++ app/models/db/DBUser.java
@@ -31,8 +31,10 @@ public class DBUser {
     public static List<User> listUsers(){
         Session session = HibernateUtilities.getSessionFactory().openSession();
         session.beginTransaction();
+
         List<User> users = session.createCriteria(User.class).list();
         session.close();
+
         return users;
     }
 
@@ -49,6 +51,71 @@ public class DBUser {
         return result.size() != 0;
     }
 
+    //EDIT USER (USER OBJECT) RET BOOLEAN
+    public static int editUser(User user) {
+        Session session = HibernateUtilities.getSessionFactory().openSession();
+        session.beginTransaction();
+        try {
+            session.update(user);
+            session.getTransaction().commit();
+            return ProjectConstants.SUCCESS;
+        } catch (Exception e){
+            e.printStackTrace();
+            return ProjectConstants.FAILED;
+        } finally {
+            session.close();
+        }
+    }
+
+    //GET_USER (INT ID) RET USER OBJECT
+    public User getUser(int id) {
+        Session session = HibernateUtilities.getSessionFactory().openSession();
+        session.beginTransaction();
+        User user= (User) session.load(User.class, new Integer(id));
+        session.close();
+        return user;
+    }
+
+    //DEACTIVATE (INT ID) RET BOOLEAN
+    public static int deactivateUser(int id) {
+        Session session = HibernateUtilities.getSessionFactory().openSession();
+        session.beginTransaction();
+        try {
+            User user= (User) session.load(User.class, new Integer(id));
+            user.setIsActive(ProjectConstants.INACTIVE);
+            editUser(user);
+            session.getTransaction().commit();
+            return ProjectConstants.SUCCESS;
+        } catch (Exception e){
+            e.printStackTrace();
+            return ProjectConstants.FAILED;
+        } finally {
+            session.close();
+        }
+    }
+
+
+    //DELETE (INT ID) RET BOOLEAN
+    public static int deleteUser (int id) {
+        Session session = HibernateUtilities.getSessionFactory().openSession();
+        session.beginTransaction();
+        try {
+            User user = (User) session.load(User.class, new Integer(id));
+            if(null != user){
+                session.delete(user);
+            }
+            session.getTransaction().commit();
+            return ProjectConstants.SUCCESS;
+        } catch (Exception e){
+            e.printStackTrace();
+            return ProjectConstants.FAILED;
+        } finally {
+            session.close();
+        }
+    }
+
+
+
 //    public static boolean getUsers(){
 ////        Session session = HibernateUtilities.getSessionFactory().openSession();
 ////        session.beginTransaction();
@@ -61,6 +128,17 @@ public class DBUser {
 //    }
 
     public static void main(String[] args) {
+        User user = new User();
+        user.setFname("jam");
+        user.setLname("render");
+        user.setUname("jr");
+        user.setPassword("1234");
+        user.setIsActive(1);
+
+        System.out.println("Insert: " + insert(user));
+
+
+
         List<User> users = listUsers();
 //        for(User u : users){
 //            System.out.println(u.getFname());
@@ -68,5 +146,9 @@ public class DBUser {
         JsonNode json = Json.toJson(users);
         System.out.println(json.toString());
 //        System.out.println(login("manrick.capotolan", "12334"));
+
+
+
+        System.out.println("Login: " + login("black","1234"));
     }
 }
